56682ac7bc6ea69f230e6423f982f596ff7d35a7
Deepanshu Gupta <312.gupta@gmail.com>
2023-03-28 18:54:47 -0400

    added `components` folder (folder structuring) and code cleanup (#2)
    - cleaned up unwanted files and linked code
    
    Co-authored-by: Deepanshu Gupta <deepanshu.gupta@binance.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+import PieChart from './components/PieChart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> BNB Circulation Details Page!</p>
+        <p> COMING SOON..!!</p>
+        <div>
+          <PieChart></PieChart>
+        </div>
       </header>
     </div>
   );

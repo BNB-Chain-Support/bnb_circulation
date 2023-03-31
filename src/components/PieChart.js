400931f75923cacf27d66da179da260f6e53f1f8
Deepanshu Gupta <deepanshu.gupta@binance.com>
2023-03-31 18:00:13 -0400

    added dynamic doughnut chart with real values and api calls

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const PieChart = () => {
+
+    const [bscSupply, setBscSupply] = useState(0);
+    const [bcSupply, setBcSupply] = useState(0);
+    const [scheduledBurn, setScheduledBurn] = useState(0);
+    const [realTimeBurn, setRealTimeBurn] = useState(0);
+    const [hackerAmtLockedAsBurn, setHackerAmtLockedAsBurn] = useState(0);
+
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                //  Get BSC and BC Supply
+                const bscBcSupplyReqData = await fetch("https://www.bnbburn.info/api/getBnbSupply",);
+                const bscBcSupplyResData = await bscBcSupplyReqData.json();
+                console.log("BSC and BC Supply");
+                console.log(bscBcSupplyResData);
+
+                //  Get Real-time burn
+                const realTimeBurnReqData = await fetch("https://www.bnbburn.info/api/getRealTimeBurnInfo");
+                const realTimeBurnResData = await realTimeBurnReqData.json();
+                console.log("real-time burn data");
+                console.log(realTimeBurnResData.totalBurnt);
+
+                //  Get all Quarterly Burns
+                const quarterlyBurnReqData = await fetch("https://www.bnbburn.info/api/getQuarterBurns");
+                const quarterlyBurnResData = await quarterlyBurnReqData.json();
+
+                //  Cal. Scheduled Burn by adding all quarterly burns
+                let scheduledBurnData = 0;
+                for (let i = 0; i < (quarterlyBurnResData?.quarters ?? []).length; i++) {
+                    const quarter = quarterlyBurnResData?.quarters[i];
+                    scheduledBurnData += parseFloat(quarter.amount);
+                }
+                console.log("Scheduled burn: ", scheduledBurnData);
+
+                //  Setting up all values
+                setBscSupply(bscBcSupplyResData?.bscSupply);
+                setBcSupply(bscBcSupplyResData?.bcSupply);
+                setRealTimeBurn(realTimeBurnResData?.totalBurnt);
+                setScheduledBurn(scheduledBurnData);
+                setHackerAmtLockedAsBurn(1020000);
+            } catch (err) {
+                console.log("Error occured: ", err);
+            }
+        };
+        getData();
+    }, [])
+
     return (
         <React.Fragment>
             <div className="container-fluid">
-                {/* Pie Chart */}
+                <h3> Pie Chart </h3>
                 <div className="row">
                     <div className="col-md-5 mb-3">
 
 
-                        <Pie
+                        <Doughnut
                             width={500}
                             height={400}
                             data={{
-                                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                                labels: ['BSC Supply', 'BC Supply', 'Scheduled Burn', 'Real-Time Burn', 'Hacker Amount locked as burn'],
                                 datasets: [
                                     {
-                                        label: '# of Votes',
-                                        data: [12, 19, 3, 5, 2, 3],
+                                        // label: '# of supply/burn',
+                                        data: [bscSupply.toFixed(2), bcSupply.toFixed(2), scheduledBurn.toFixed(2), realTimeBurn.toFixed(2), hackerAmtLockedAsBurn.toFixed(2)],
                                         backgroundColor: [
                                             'rgba(255, 99, 132, 0.2)',
                                             'rgba(54, 162, 235, 0.2)',
@@ -38,6 +86,10 @@ const PieChart = () => {
                                             'rgba(255, 159, 64, 1)',
                                         ],
                                         borderWidth: 1,
+                                        hoverOffset: 20,
+                                        // offset: [
+                                        //     0, 0, 0, 0, 0, 0
+                                        // ]
                                     },
                                 ],
                             }}
@@ -46,8 +98,19 @@ const PieChart = () => {
                                 responsive: true,
                                 plugins: {
                                     title: {
+                                        fontSize: 30,
                                         text: 'BNB Supply breakdown',
-                                        display: true
+                                        display: true,
+                                        font: {
+                                            size: 20
+                                        }
+                                    },
+                                    legend: {
+                                        labels: {
+                                            font: {
+                                                size: 15
+                                            }
+                                        }
                                     }
                                 }
                             }}

d4201db29ebff84883d00a8e0d8933e5876c1289
Deepanshu Gupta <312.gupta@gmail.com>
2023-04-07 18:51:42 -0400

    - code cleanup (removed console.logs) and added common utils (#5)
    - removed all console.logs and code cleanup
    - modified all BNB value to upto 2 decimals places
    - added utils file to be used across application
    
    Co-authored-by: Deepanshu Gupta <deepanshu.gupta@binance.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import SimpleCard from "./SimpleCard";
 import DoughnutChart from "./DoughnutChart";
+import { toFixed } from "../utils/utils";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Home = () => {
@@ -14,21 +15,16 @@ const Home = () => {
     const [hackerAmtLockedAsBurn, setHackerAmtLockedAsBurn] = useState(0);
     const [communityBurn, setCommunityBurn] = useState(0);
 
-
     useEffect(() => {
         const getData = async () => {
             try {
                 //  Get BSC and BC Supply
                 const bscBcSupplyReqData = await fetch("https://www.bnbburn.info/api/getBnbSupply",);
                 const bscBcSupplyResData = await bscBcSupplyReqData.json();
-                console.log("BSC and BC Supply");
-                console.log(bscBcSupplyResData);
 
                 //  Get Real-time burn
                 const realTimeBurnReqData = await fetch("https://www.bnbburn.info/api/getRealTimeBurnInfo");
                 const realTimeBurnResData = await realTimeBurnReqData.json();
-                console.log("real-time burn data");
-                console.log(realTimeBurnResData.totalBurnt);
 
                 //  Get all Quarterly Burns
                 const quarterlyBurnReqData = await fetch("https://www.bnbburn.info/api/getQuarterBurns");
@@ -43,37 +39,18 @@ const Home = () => {
                         scheduledBurnData += parseFloat(quarter.amount);
                     }
                 }
-                console.log("Scheduled burn: ", scheduledBurnData);
 
                 //  Setting up all values
                 setBscSupply(bscBcSupplyResData?.bscSupply);
-
-                console.log("======= Setting bsc supply =======");
-                console.log("bscBcSupplyResData: ", bscBcSupplyResData);
-                console.log("bscBcSupplyResData?.bscSupply: ", bscBcSupplyResData?.bscSupply);
-                console.log("bsc supply: ", bscSupply);
                 setBcSupply(bscBcSupplyResData?.bcSupply);
                 setRealTimeBurn(realTimeBurnResData?.totalBurnt);
                 setScheduledBurn(scheduledBurnData);
                 setHackerAmtLockedAsBurn(1020000);
 
-                console.log("1st 4 params: ", parseFloat("200000000") + parseFloat("2000000") - parseFloat(bscBcSupplyResData?.bscSupply).toFixed(2) - parseFloat(bscBcSupplyResData?.bcSupply).toFixed(2));
-                console.log("1st 5 params: ", parseFloat("200000000") + parseFloat("2000000") - parseFloat(bscBcSupplyResData?.bscSupply).toFixed(2) - parseFloat(bscBcSupplyResData?.bcSupply).toFixed(2) - parseFloat("1020000"));
-                console.log("1st 6 params: ", parseFloat("200000000") + parseFloat("2000000") - parseFloat(bscBcSupplyResData?.bscSupply).toFixed(2) - parseFloat(bscBcSupplyResData?.bcSupply).toFixed(2) - parseFloat("1020000") - parseFloat(realTimeBurnResData?.totalBurnt).toFixed(2));
-                console.log("parseFloat(bscSupply).toFixed(2): ", parseFloat(bscBcSupplyResData.bscSupply).toFixed(2));
-                console.log("parseFloat(bc).toFixed(2): ", parseFloat(bscBcSupplyResData.bcSupply).toFixed(2));
-                console.log("parseFloat(hacker).toFixed(2): ", parseFloat("1020000"));
-                console.log("parseFloat(real-time).toFixed(2): ", parseFloat(realTimeBurnResData?.totalBurnt).toFixed(2));
-                console.log("parseFloat(scheduledBurnData).toFixed(2): ", parseFloat(scheduledBurnData).toFixed(2));
+                // const communityBurn = parseFloat("200000000") + parseFloat("2000000") - parseFloat(bscBcSupplyResData?.bscSupply).toFixed(2) - parseFloat(bscBcSupplyResData?.bcSupply).toFixed(2) - parseFloat("1020000") - parseFloat(realTimeBurnResData?.totalBurnt).toFixed(2) - parseFloat(scheduledBurnData).toFixed(2);
+                const communityBurn = parseFloat("200000000") + parseFloat("2000000") - toFixed(bscBcSupplyResData?.bscSupply, 2) - toFixed(bscBcSupplyResData?.bcSupply, 2) - parseFloat("1020000") - toFixed(realTimeBurnResData?.totalBurnt, 2) - toFixed(scheduledBurnData, 2)
+                setCommunityBurn(communityBurn);
 
-                const adds = parseFloat("200000000") + parseFloat("2000000") - parseFloat(bscBcSupplyResData?.bscSupply).toFixed(2) - parseFloat(bscBcSupplyResData?.bcSupply).toFixed(2) - parseFloat("1020000") - parseFloat(realTimeBurnResData?.totalBurnt).toFixed(2) - parseFloat(scheduledBurnData).toFixed(2);
-                // console.log("check: ", parseFloat(hackerAmtLockedAsBurn.toString()))
-                // const subs = 
-
-                console.log("adds: ", adds);
-                // console.log("subs: ", subs);
-
-                setCommunityBurn(adds);
             } catch (err) {
                 console.log("Error occured: ", err);
             }
@@ -84,24 +61,22 @@ const Home = () => {
     return (
         <React.Fragment>
             <div className="container-fluid">
-                <h3> Pie Chart </h3>
                 <div className="row">
                     <div className="col-md-5 mb-3">
 
                         <DoughnutChart dataArrProp={[bscSupply.toFixed(2), bcSupply.toFixed(2), scheduledBurn.toFixed(2), realTimeBurn.toFixed(2), hackerAmtLockedAsBurn.toFixed(2)]} textArrProp={['BSC Supply', 'BC Supply', 'Scheduled Burn', 'Real-Time Burn', 'Hacker Amount locked as burn']}></DoughnutChart>
-
 
                     </div>
                 </div>
 
                 {/* Cards for BSC, BC, Real-time burn, SB, HM locked as Burn */}
                 <div>
-                    <SimpleCard valueProp={bscSupply} textProp={"BSC Supply"}></SimpleCard>
-                    <SimpleCard valueProp={bcSupply} textProp={"BC Supply"}></SimpleCard>
-                    <SimpleCard valueProp={scheduledBurn} textProp={"Schedule Burn"}></SimpleCard>
-                    <SimpleCard valueProp={realTimeBurn} textProp={"Real-Time Burn"}></SimpleCard>
-                    <SimpleCard valueProp={communityBurn} textProp={"Community Burn"}></SimpleCard>
-                    <SimpleCard valueProp={hackerAmtLockedAsBurn} textProp={"Hacker Amt lkd as burn"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(bscSupply, 2)} textProp={"BSC Supply"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(bcSupply, 2)} textProp={"BC Supply"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(scheduledBurn, 2)} textProp={"Schedule Burn"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(realTimeBurn, 2)} textProp={"Real-Time Burn"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(communityBurn, 2)} textProp={"Community Burn"}></SimpleCard>
+                    <SimpleCard valueProp={toFixed(hackerAmtLockedAsBurn, 0)} textProp={"Hacker amt locked"}></SimpleCard>
                 </div>
 
             </div>
---
+++ src/utils/utils.js
@@ -0,0 +1,3 @@
+export const toFixed = (valueInStr, fixedUptoDecimals) => {
+    return parseFloat(valueInStr).toFixed(fixedUptoDecimals);
+}
